c0c61c57d6da717bfdb74c5024828bea459c9935
Mari-Ogawa <awashimauranaibu@gmail.com>
2018-05-26 11:22:23 +0900

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
   def following?(other_user)
      relationships.find_by(followed_id: other_user.id)
   end
@@ -28,6 +32,10 @@ class User < ApplicationRecord
 
   def unfollow!(other_user)
   relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
   end
 
 end
